99fe1bc3942d0efc88b1dd9296904291e5e7344d
Shreya Verma <shreyaverma@Shreyas-MacBook-Pro.local>
2023-10-13 17:22:08 +0530

    testing CICD

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import numpy as np
 import pandas as pd
 from mlProject.pipeline.prediction import PredictionPipeline
 
-app = Flask(__name__) # initializing a flask app
+app = Flask(__name__) # initializing a flask
 
 @app.route('/',methods=['GET'])  # route to display the home page
 def homePage():
-    print('Hello, please enter your values')
+    # print('Hello, please enter your values')
     return render_template("index.html")
 
 
